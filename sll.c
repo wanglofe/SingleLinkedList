84c0808d969f0e7a80c488801d71749a8d2a9dd0
wanglofe <562008105@qq.com>
2018-05-15 04:15:11 -0500

    Add some sll operations.

--- sll.c
+++ sll.c
@@ -17,26 +17,126 @@ int sll_insert(sll_t **sll, int val)
 	if(!(*sll)) {
 		*sll = newnode;	
 	} else {
-	//	sll_t *tmp = (*sll)->next;
-	//	while(tmp) {
-	//		printf("tmp-> %p, %d\n", tmp, tmp->val);
-	//		tmp = tmp->next;
-	//	}
-	//	tmp = newnode;
-		sll_t *tmp = (*sll);
-		while (tmp->next){
+//		sll_t *tmp = (*sll)->next;
+//		while (tmp){
+//			tmp = tmp->next;
+//		}
+//		tmp = newnode;
+		sll_t *tmp = *sll;
+		while(tmp->next) {
 			tmp = tmp->next;
 		}
 		tmp->next = newnode;
-	
 	}
 	return 0;
+}
+
+int sll_delete(sll_t **sll, int val)
+{
+	if(!(*sll)) {
+		return -1;
+	}
+	sll_t *tmp = *sll;
+	if((*sll)->val == val /*at head*/) {
+		(*sll) = (*sll)->next;
+		free(tmp);
+	}
+	sll_t *previous = NULL;
+	while(tmp) {
+		if(val == tmp->val) {
+			break;
+		}
+		previous = tmp;
+		tmp = tmp->next;
+	}
+	if(!tmp/*no found*/) {
+		return -1;
+	} else if(tmp->next == NULL/*at end*/) {
+		previous->next = NULL;	
+	} else {
+		previous->next = tmp->next;	
+	}
+	free(tmp);
+	return 0;
+}
+
+int sll_destory(sll_t *sll)
+{
+	sll_t *tmp = sll;
+	while(sll) {
+		tmp = sll;
+		sll = sll->next;
+		free(tmp);
+	}
+	return 0;
+}
+
+sll_t *sll_find(sll_t *sll, int val)
+{
+	sll_t *tmp = sll;
+	if(!sll) {
+		return NULL;
+	}
+	while(tmp) {
+		if(tmp->val == val) {
+			return tmp;
+		}
+		tmp = tmp->next;
+	}
+	return NULL;
+}
+
+int sll_replace(sll_t *sll, int origin, int after)
+{
+	if(!sll) {
+		return -1;
+	}
+	sll_t *node = sll_find(sll, origin); 
+	if(!node) {
+		return -1;
+	} else {
+		node->val = after;
+		return 0;
+	}
+}
+
+sll_t *sll_reverse(sll_t *sll)
+{
+	sll_t *reversed_sll = NULL;
+	sll_t *current = sll, *previous = NULL, *next = NULL;
+	if(!sll) {
+		return NULL;
+	}
+	while(current) {
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
+	}
+	reversed_sll = previous;
+	return reversed_sll;
+}
+
+size_t sll_length(sll_t *sll)
+{
+	sll_t *tmp = sll;
+	size_t length = 0;
+	if(!sll) {
+	
+	} else {
+		while(tmp) {
+			++length;
+			tmp = tmp->next;
+		}
+	}
+	return length;
 }
 
 void sll_print(sll_t *sll)
 {
 	sll_t *tmp = sll;
 	while(tmp) {
+		printf("%d ", tmp->val);
 		tmp = tmp->next;
 	}
 	putchar('\n');
@@ -50,5 +150,19 @@ int main(void)
 		sll_insert(&sll, i);
 	}
 	sll_print(sll);
+
+	printf("sll length: %d\n", sll_length(sll));
+//	sll_print(sll_reverse(sll));
+
+	sll_delete(&sll, 10);
+	sll_delete(&sll, 0);
+	sll_delete(&sll, 19);
+	sll_print(sll);
+
+	sll_replace(sll, 5, 500);
+	sll_print(sll);
+
+	sll_destory(sll);
+
 	return 0;
 }
